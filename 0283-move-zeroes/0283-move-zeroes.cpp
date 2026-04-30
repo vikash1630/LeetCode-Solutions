32e9bba7a3a25ccf348f6b3c3b61b2d2a187745d
vikash1630 <m.vikash1630@gmail.com>
2026-04-30 11:50:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int cnt = 0;
         int n = nums.size();
-        int i = 0;
-        while (i<n) {
-            if (nums[i] == 0) {
-                cnt++;
-            }
-            i++;
-        }
         int j = 0;
-        i = 0;
-        while (i<n) {
-            if (j == n - cnt) break;
+        for (int i = 0;i<n;i++) {
             if (nums[i] != 0) {
-                nums[j] = nums[i];
+                swap(nums[i],nums[j]);
                 j++;
             }
-            i++;
         }
-        while (j<n) {
-            nums[j] = 0;
-            j++;
-        } 
+
+
+        // Brute Force
+        // int cnt = 0;
+        // int n = nums.size();
+        // int i = 0;
+        // while (i<n) {
+        //     if (nums[i] == 0) {
+        //         cnt++;
+        //     }
+        //     i++;
+        // }
+        // int j = 0;
+        // i = 0;
+        // while (i<n) {
+        //     if (j == n - cnt) break;
+        //     if (nums[i] != 0) {
+        //         nums[j] = nums[i];
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // while (j<n) {
+        //     nums[j] = 0;
+        //     j++;
+        // } 
     }
 };
