47919d9b68b711ba84d7ab160bf4d0567751691d
vikash1630 <m.vikash1630@gmail.com>
2026-04-30 12:24:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i = 1;
-        int n = nums.size();
-        
-        int temp = nums[0];
-        while(i<n) {
-            if (nums[i] != temp) {
-                return temp;
-            }
-            temp = nums[i + 1];
-            i = i + 2;
+
+        // XOR as XOR cancels duplicates
+        // a ^ a = 0 and 0 ^ a = a so pairs get cancelled and single number stays back
+
+        int ans = 0;
+        for (int i = 0;i<nums.size();i++) {
+            ans = ans ^ nums[i];
         }
-        return temp;
+        return ans;
+        
+        // Brute Force
+        // sort(nums.begin(),nums.end());
+        // int i = 1;
+        // int n = nums.size();
+        
+        // int temp = nums[0];
+        // while(i<n) {
+        //     if (nums[i] != temp) {
+        //         return temp;
+        //     }
+        //     temp = nums[i + 1];
+        //     i = i + 2;
+        // }
+        // return temp;
     }
 };
