dac8e72c8098444d61d7aef446f0e17c639820fd
vikash1630 <m.vikash1630@gmail.com>
2026-04-30 11:40:10 +0530

    Time: 0 ms (100%), Space: 29.7 MB (40.83%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,57 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
         int n = nums.size();
         k = k%n;
-        int i = n - k;
-        vector<int> ans(n);
-        while (i<n) {
-            ans[i - (n-k)] = nums[i];
-            i++;
+        int left = 0;
+        int right = n - 1;
+        while (left<=right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
-        int j = i - (n-k);
-        i = 0;
-        while (i<n-k) {
-            ans[j] = nums[i];
-            i++;
-            j++;
+        left = 0;
+        right = k - 1;
+        while (left<=right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
-        nums = ans;
+        left = k;
+        right = n - 1;
+        while (left<=right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+
+
+
+        // Brute Force
+        // int n = nums.size();
+        // k = k%n;
+        // int i = n - k;
+        // vector<int> ans(n);
+        // while (i<n) {
+        //     ans[i - (n-k)] = nums[i];
+        //     i++;
+        // }
+        // int j = i - (n-k);
+        // i = 0;
+        // while (i<n-k) {
+        //     ans[j] = nums[i];
+        //     i++;
+        //     j++;
+        // }
+        // nums = ans;
         
     }
+
 };
