9305e221c980ca350edee406ba09694cc8e5e8d0
vikash1630 <m.vikash1630@gmail.com>
2026-04-30 15:53:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> temp = nums;
-        int left = 0;
+        
+        unordered_map<int,int> mpp;
         int n = nums.size();
-        int right = n - 1;
-        sort(temp.begin(),temp.end());
-        while (left < right) {
-            int sum = temp[left] + temp[right];
-            if (sum == target) break;
-            if (sum < target) left++;
-            else right--;
-        }
-
-        int a = -1;
-        int b = -1;
         for (int i = 0;i<n;i++) {
-            if (nums[i] == temp[left] || nums[i] == temp[right]) {
-                if (a == -1) {
-                    a = i;
-                    continue;
-                }
-                b = i;
-                break;
-            }
+            int sum = nums[i];
+            if (mpp.find(target - sum) != mpp.end()) return {mpp[target-sum],i};
+            mpp[sum] = i;
         }
+        return {-1,-1};
+        
+        //  Brute Force
+        // vector<int> temp = nums;
+        // int left = 0;
+        // int n = nums.size();
+        // int right = n - 1;
+        // sort(temp.begin(),temp.end());
+        // while (left < right) {
+        //     int sum = temp[left] + temp[right];
+        //     if (sum == target) break;
+        //     if (sum < target) left++;
+        //     else right--;
+        // }
 
-        return {a,b};
+        // int a = -1;
+        // int b = -1;
+        // for (int i = 0;i<n;i++) {
+        //     if (nums[i] == temp[left] || nums[i] == temp[right]) {
+        //         if (a == -1) {
+        //             a = i;
+        //             continue;
+        //         }
+        //         b = i;
+        //         break;
+        //     }
+        // }
+
+        // return {a,b};
     }
 };
