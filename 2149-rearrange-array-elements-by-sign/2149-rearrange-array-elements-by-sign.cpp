813f87d78964b7ae25e2ff98c9cf103fea1833f8
vikash1630 <m.vikash1630@gmail.com>
2026-04-30 17:58:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
-        for (int i = 0;i<nums.size();i++)  {
+        int n = nums.size();
+        vector<int> ans(n);
+        int posInd = 0;
+        int negInd = 1;
+        for (int i = 0;i<n;i++) {
             if (nums[i] < 0) {
-                neg.push_back(nums[i]);
+                ans[negInd] = nums[i];
+                negInd += 2;
             }
             else {
-                pos.push_back(nums[i]);
+                ans[posInd] = nums[i];
+                posInd += 2;
             }
         }
-        int j = 0;
-        int k = 0;
-        for (int i = 0;i<nums.size();i++) {
-            if (i%2 == 0) {
-                nums[i] = pos[j];
-                j++;
-            }
-            else {
-                nums[i] = neg[k];
-                k++;
-            }
-        }
-        return nums;
+
+        return ans;
+
+
+        // Brute Force
+        // vector<int> pos;
+        // vector<int> neg;
+        // for (int i = 0;i<nums.size();i++)  {
+        //     if (nums[i] < 0) {
+        //         neg.push_back(nums[i]);
+        //     }
+        //     else {
+        //         pos.push_back(nums[i]);
+        //     }
+        // }
+        // int j = 0;
+        // int k = 0;
+        // for (int i = 0;i<nums.size();i++) {
+        //     if (i%2 == 0) {
+        //         nums[i] = pos[j];
+        //         j++;
+        //     }
+        //     else {
+        //         nums[i] = neg[k];
+        //         k++;
+        //     }
+        // }
+        // return nums;
     }
 };
