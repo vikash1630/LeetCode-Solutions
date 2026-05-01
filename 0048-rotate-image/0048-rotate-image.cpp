7004a6c56d24f9eeabd4881f5dc1e71e744761ec
vikash1630 <m.vikash1630@gmail.com>
2026-05-01 15:10:09 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.36%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp = matrix;
         int n = matrix.size();
-        int i = 0;
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = temp[n - 1 - j][i];
-        }
-    }
-    
 
+        // Transpose the Matrix across diagonals
+        for (int i = 0;i<n;i++) {
+            for (int j = 0;j<i;j++) {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        // Now Reverse Each row and our required Matrix is ready therefore rotated 90 degree clockwise
+        for (int i = 0;i<n;i++) {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
