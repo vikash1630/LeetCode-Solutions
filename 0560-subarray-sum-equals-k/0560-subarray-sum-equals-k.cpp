bdf1d0874944f036829b4e88e015133a9cfd3f6b
vikash1630 <m.vikash1630@gmail.com>
2026-05-01 15:37:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -8,6 +8,8 @@ public:
         for (int i = 0;i<n;i++) {
             prefixSum += nums[i];
             if (k == prefixSum) ans++;
+            // Why ans += mpp[prefixSum - k] instead of ans++   => because we are counting possible number of sub arrays that means how many times the diffrence (prefixSum - k) appears that many more possible subarrays
+            
             if (mpp.find(prefixSum - k) != mpp.end()) ans += mpp[prefixSum - k];
             mpp[prefixSum]++;
         }
