c531fd3d434e4a6c50e310e070ac75630e26550b
vikash1630 <m.vikash1630@gmail.com>
2026-05-03 14:14:38 +0530

    Time: 0 ms (100%), Space: 26.1 MB (92.83%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            // Check Which half to discard 
+            if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
+                // Mid is double with its pair at right
+                if ((mid+1)%2 == 0) {
+                    // Means right half elements are paired 
+                    // Now single element in right half
+                    high = mid - 1;
+                }
+                else {
+                    // Means right half are odd number of elements
+                    low = mid + 1;
+                }
+            }
+            else if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
+                // Mid is double with its pair at left
+                if (mid%2 == 0) {
+                    // Means right half element are paired
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                return nums[mid];
+            }
+        }
+        return -1;
+    }
+};
