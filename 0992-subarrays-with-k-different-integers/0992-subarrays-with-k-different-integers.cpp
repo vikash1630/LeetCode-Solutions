d4fbe3767adeb5fb2c9f231ec6ed15cd444465c9
vikash1630 <m.vikash1630@gmail.com>
2026-06-23 18:25:35 +0530

    Time: 85 ms (49.85%), Space: 56.3 MB (19.36%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,29 +1,29 @@
 class Solution {
-public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return solve(nums,k) - solve(nums,k - 1);
-    }
-    
+private:
     int solve(vector<int>& nums, int k) {
-        if (k <= 0) return 0;
-        int left = 0;
         int n = nums.size();
-        int right = 0;
-        int cnt = 0;
-        unordered_map<int,int> mpp;
-        while (right < n) {
-            mpp[nums[right]]++;
-            while (left < right && mpp.size() > k) {
-                mpp[nums[left]]--;
-                if (mpp[nums[left]] == 0) {
-                    mpp.erase(nums[left]);
+        int i = 0;
+        int j = 0;
+        unordered_map<int, int> mpp;
+        int count = 0;
+        while (j < n) {
+            mpp[nums[j]]++;
+            while (mpp.size() > k) {
+                mpp[nums[i]]--;
+                if (mpp[nums[i]] == 0) {
+                    mpp.erase(nums[i]);
                 }
-                left++;
+                i++;
             }
-            cnt = cnt + (right - left + 1);
-            right++;
+            count += j - i + 1;
+            j++;
         }
-        return cnt;
+        return count;
     }
 
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        return solve(nums,k) - solve(nums,k - 1);
+    }
 };
