5cf7be60cf9f406c7a6d74ab7859bb8e2effb5d5
vikash1630 <m.vikash1630@gmail.com>
2026-07-25 00:53:52 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,11 +17,24 @@ private:
         return dp[n] = MemClimbStairs(n - 1, dp) + MemClimbStairs(n - 2, dp);
     }
 
+// Tabulation
+    int TabClimbStairs(int n, vector<int> &dp) {
+        if (n == 1) return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2;i<=n;i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
 public:
     int climbStairs(int n) {
         // return RecClimbStairs(n);
         // n + 1 since 0 is also there and n also counted
         vector<int> dp(n + 1, -1);
-        return MemClimbStairs(n, dp);
+        // return MemClimbStairs(n, dp);
+        return TabClimbStairs(n, dp);
     }
 };
