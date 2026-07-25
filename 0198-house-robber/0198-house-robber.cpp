5c53c02b5cad989bfc502ff77d1beb3275c0a8c9
vikash1630 <m.vikash1630@gmail.com>
2026-07-25 21:47:48 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -21,11 +21,27 @@ private:
         int dontTake = mem(nums, dp, n, ind + 1); 
         return dp[ind] = max(take, dontTake);
     }
+
+    // Tabulation
+    int tab(vector<int>& nums, int n) {
+    vector<int> dp(n + 2, 0);
+
+    for (int i = n - 1; i >= 0; i--) {
+        int take = nums[i] + dp[i + 2];
+        int dontTake = dp[i + 1];
+
+        dp[i] = max(take, dontTake);
+    }
+
+    return dp[0];
+}
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         // return rec(nums, n, 0);
         vector<int> dp(n, -1);
-        return mem(nums, dp, n, 0);
+        // return mem(nums, dp, n, 0);
+        return tab(nums, n);
     }
 };
