d4d5681964549a7ec5f4ddf09c2403822237cc87
vikash1630 <m.vikash1630@gmail.com>
2026-07-26 22:25:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.54%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,64 @@
 class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-
-        int n = triangle.size();
-
-        // Last row is our base case
-        vector<int> front = triangle[n - 1];
-
-        // Start from second last row
-        for (int i = n - 2; i >= 0; i--) {
-
-            vector<int> cur(i + 1);
-
-            for (int j = 0; j <= i; j++) {
-
-                int down = triangle[i][j] + front[j];
-
-                int diagonal = triangle[i][j] + front[j + 1];
-
-                cur[j] = min(down, diagonal);
-            }
-
-            front = cur;
+private:
+    // Recurrsion
+    int Rec(int n, vector<vector<int>>& tri, int r, int ind) {
+        if (r == n - 1) {
+            return tri[r][ind];
         }
 
-        return front[0];
+        // Go with i
+        int a = tri[r][ind] + Rec(n, tri, r + 1, ind);
+
+        // Go with i + 1
+        int b = tri[r][ind] + Rec(n, tri, r + 1, ind + 1);
+
+        return min(a, b);
+    }
+
+    // Memoization
+    int Mem(int n, vector<vector<int>>& tri, int r, int ind, vector<vector<int>> &dp) {
+        if (r == n - 1)
+            return tri[r][ind];
+
+        if (dp[r][ind] != -1)
+            return dp[r][ind];
+
+        int down = tri[r][ind] + Mem(n, tri, r + 1, ind, dp);
+        int diag = tri[r][ind] + Mem(n, tri, r + 1, ind + 1, dp);
+
+        return dp[r][ind] = min(down, diag);
+
+    }
+
+    // Tabulation
+    int Tab(int n, vector<vector<int>>& tri, vector<vector<int>>& dp) {
+
+    // Base case
+    for (int i = 0; i < dp[n - 1].size(); i++)
+        dp[n - 1][i] = tri[n - 1][i];
+
+    // Fill DP from bottom to top
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < tri[i].size(); j++) {
+
+            int down = tri[i][j] + dp[i + 1][j];
+            int diag = tri[i][j] + dp[i + 1][j + 1];
+
+            dp[i][j] = min(down, diag);
+        }
+    }
+
+    return dp[0][0];
+}
+
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (n == 1) return triangle[0][0];
+        // return Rec(n, triangle, 0, 0);
+        vector<vector<int>> dp(n);
+        for (int i = 0; i < n; i++) dp[i].resize(i + 1, -1);
+        // return Mem(n, triangle, 0, 0,dp);
+        return Tab(n, triangle, dp);
     }
 };
