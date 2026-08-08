b4c8cb515b4109cbf16d230252ab5d820343df97
vikash1630 <m.vikash1630@gmail.com>
2026-08-08 15:43:55 +0530

    Time: 19 ms (13.22%), Space: 22.8 MB (5.05%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -12,10 +12,10 @@ private:
         if (s1[i] == s2[j]) return dp[i][j] = 1 + Mem(s1, s2, i - 1, j - 1, dp);
         return dp[i][j] = max(Mem(s1, s2, i - 1, j, dp), Mem(s1, s2, i, j - 1, dp));
     }
-    int Tab(string s1, string s2, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    int Tab(string s1, string s2, int n, int m) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         for (int i = 1;i<=n;i++) {
-            for (int j = 1;j<=n;j++) {
+            for (int j = 1;j<=m;j++) {
                 if (s1[i - 1] == s2[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
@@ -24,12 +24,12 @@ private:
                 }
             }
         }
-        return dp[n][n];
+        return dp[n][m];
     }
-    int opt(string s1, string s2, int n) {
-        vector<int> dp(n + 1, 0);
+    int opt(string s1, string s2, int n, int m) {
+        vector<int> dp(m + 1, 0);
         for (int i = 1;i<=n;i++) {
-            vector<int> dum(n + 1, 0);
+            vector<int> dum(m + 1, 0);
             for (int j = 1;j<=n;j++) {
                 if (s1[i - 1] == s2[j - 1]) {
                     dum[j] = 1 + dp[j - 1];
@@ -40,7 +40,7 @@ private:
             }
             dp = dum;
         }
-        return dp[n];
+        return dp[m];
     }
 public:
     int minDistance(string word1, string word2) {
@@ -51,7 +51,8 @@ public:
         // common = 2 * Rec(word1, word2, n - 1, m - 1);
 
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        common = 2 * Mem(word1, word2, n - 1, m - 1, dp);
+        // common = 2 * Mem(word1, word2, n - 1, m - 1, dp);
+        common = 2 * Tab(word1, word2, n, m);
         
 
         
