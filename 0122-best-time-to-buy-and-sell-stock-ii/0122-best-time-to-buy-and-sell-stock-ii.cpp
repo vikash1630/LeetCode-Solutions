b550d3b9f078462d77f6a900f3973741d8c729d8
vikash1630 <m.vikash1630@gmail.com>
2026-08-09 06:46:13 +0530

    Time: 15 ms (14.9%), Space: 24.8 MB (30.34%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,11 +6,11 @@ private:
         long profit = 0;
         // We Can buy
         if (buy == 0) {
-            profit = max(0 + Rec(prices, ind + 1, n, buy), -prices[ind] + Rec(prices, ind + 1, n, buy - 1));
+            profit = max(0 + Rec(prices, ind + 1, n, buy), -prices[ind] + Rec(prices, ind + 1, n, buy + 1));
         }
         // We can sell
         else {
-            profit = max(0 + Rec(prices, ind + 1, n, buy), prices[ind] + Rec(prices, ind + 1, n, buy + 1));
+            profit = max(0 + Rec(prices, ind + 1, n, buy), prices[ind] + Rec(prices, ind + 1, n, buy - 1));
         }
         return profit;
     }
@@ -28,11 +28,32 @@ private:
         }
         return dp[ind][buy] = profit;
     }
+
+    int Tab(vector<int>& prices, int ind, int n, int buy, vector<vector<long>> &dp) {
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for (int i = n - 1;i>=0;i--) {
+            long profit = 0;
+            for (int buy = 0;buy<=1;buy++) {
+                // We Can buy
+                if (buy == 0) {
+                    profit = max(0 + dp[i + 1][buy], -prices[i] + dp[i + 1][buy + 1]);
+                }
+                // We can sell
+                else {
+                    profit = max(0 + dp[i + 1][buy], prices[i] + dp[i + 1][buy - 1]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][0];
+    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         // return Rec(prices, 0, n, 0);
-        vector<vector<long>> dp(n, vector<long>(2, -1));
-        return Mem(prices, 0, n, 0, dp);
+        vector<vector<long>> dp(n + 1, vector<long>(2, -1));
+        // return Mem(prices, 0, n, 0, dp);
+        return Tab(prices, 0, n, 0, dp);
     }
 };
