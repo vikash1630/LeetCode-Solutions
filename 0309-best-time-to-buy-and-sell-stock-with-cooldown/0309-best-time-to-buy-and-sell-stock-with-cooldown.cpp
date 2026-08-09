38b0033654ff250f3dba352710917dcd3049a49b
vikash1630 <m.vikash1630@gmail.com>
2026-08-09 12:28:33 +0530

    Time: 166 ms (5.34%), Space: 94.4 MB (5.1%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,37 +1,62 @@
 class Solution {
-int Rec(vector<int> &prices, int ind, int n, int buy) {
-    if (ind >= n) return 0;
+    int Rec(vector<int>& prices, int ind, int n, int buy) {
+        if (ind >= n)
+            return 0;
 
-    // Buy
-    if (buy%2 == 0) {
-        int take = -prices[ind] + Rec(prices, ind + 1, n, buy + 1);
-        int donttake = Rec(prices, ind + 1, n, buy);
-        return max(take, donttake);
+        // Buy
+        if (buy % 2 == 0) {
+            int take = -prices[ind] + Rec(prices, ind + 1, n, buy + 1);
+            int donttake = Rec(prices, ind + 1, n, buy);
+            return max(take, donttake);
+        }
+        // Sell
+        else {
+            int sell = prices[ind] + Rec(prices, ind + 2, n, buy + 1);
+            int dontsell = Rec(prices, ind + 1, n, buy);
+            return max(sell, dontsell);
+        }
     }
-    // Sell
-    else {
-        int sell = prices[ind] + Rec(prices, ind + 2, n, buy + 1);
-        int dontsell = Rec(prices, ind + 1, n, buy);
-        return max(sell, dontsell);
-    }
-}
 
-int Mem(vector<int> &prices, int ind, int n, int buy, vector<vector<int>> &dp) {
-    if (ind >= n) return 0;
-    if (dp[ind][buy] != -1) return dp[ind][buy];
-    // Buy
-    if (buy%2 == 0) {
-        int take = -prices[ind] + Mem(prices, ind + 1, n, buy + 1, dp);
-        int donttake = Mem(prices, ind + 1, n, buy, dp);
-        return dp[ind][buy] = max(take, donttake);
+    int Mem(vector<int>& prices, int ind, int n, int buy,
+            vector<vector<int>>& dp) {
+        if (ind >= n)
+            return 0;
+        if (dp[ind][buy] != -1)
+            return dp[ind][buy];
+        // Buy
+        if (buy % 2 == 0) {
+            int take = -prices[ind] + Mem(prices, ind + 1, n, buy + 1, dp);
+            int donttake = Mem(prices, ind + 1, n, buy, dp);
+            return dp[ind][buy] = max(take, donttake);
+        }
+        // Sell
+        else {
+            int sell = prices[ind] + Mem(prices, ind + 2, n, buy + 1, dp);
+            int dontsell = Mem(prices, ind + 1, n, buy, dp);
+            return dp[ind][buy] = max(sell, dontsell);
+        }
     }
-    // Sell
-    else {
-        int sell = prices[ind] + Mem(prices, ind + 2, n, buy + 1, dp);
-        int dontsell = Mem(prices, ind + 1, n, buy, dp);
-        return dp[ind][buy] = max(sell, dontsell);
+
+    int Tab(vector<int>& prices, int n) {
+        vector<vector<int>> dp(n + 2, vector<int>(n + 1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy < n; buy++) {
+                // Buy
+                if (buy % 2 == 0) {
+                    int take = -prices[ind] + dp[ind + 1][buy + 1];
+                    int donttake = dp[ind + 1][buy];
+                    return dp[ind][buy] = max(take, donttake);
+                }
+                // Sell
+                else {
+                    int sell = prices[ind] + dp[ind + 2][buy + 1];
+                    int dontsell = dp[ind + 1][buy];
+                    dp[ind][buy] = max(sell, dontsell);
+                }
+            }
+        }
+        return dp[0][0];
     }
-}
 
 public:
     int maxProfit(vector<int>& prices) {
