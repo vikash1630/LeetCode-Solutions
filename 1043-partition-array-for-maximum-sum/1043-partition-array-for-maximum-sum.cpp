f2277eed092f6ed90f55c5fef3af8fda477acf77
Vikash Mundakar <m.vikash1630@gmail.com>
2026-08-15 22:39:05 +0530

    Time: 0 ms (100%), Space: 11.7 MB (99.41%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,11 +1,13 @@
 class Solution {
 private:
-    int f(vector<int> &arr, int n, int k, int ind, vector<int> &dp) {
-        if (ind == n) return 0;
-        if (dp[ind] != -1) return dp[ind];
+    int f(vector<int>& arr, int n, int k, int ind, vector<int>& dp) {
+        if (ind == n)
+            return 0;
+        if (dp[ind] != -1)
+            return dp[ind];
         int maxiEle = INT_MIN;
         int ans = INT_MIN;
-        for (int i = ind;i<min(n, ind + k);i++) {
+        for (int i = ind; i < min(n, ind + k); i++) {
             maxiEle = max(maxiEle, arr[i]);
             int len = i - ind + 1;
             int sum = (len * maxiEle) + f(arr, n, k, i + 1, dp);
@@ -13,10 +15,29 @@ private:
         }
         return dp[ind] = ans;
     }
+
+    int f(vector<int>& arr, int n, int k) {
+        int dp[n + 1];
+        dp[n] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int maxiEle = INT_MIN;
+            int ans = INT_MIN;
+            for (int i = ind; i < min(n, ind + k); i++) {
+                maxiEle = max(maxiEle, arr[i]);
+                int len = i - ind + 1;
+                int sum = (len * maxiEle) + dp[i + 1];
+                ans = max(sum, ans);
+            }
+            dp[ind] = ans;
+        }
+        return dp[0];
+    }
+
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n, -1);
-        return f(arr, n, k, 0, dp);
+        // vector<int> dp(n, -1);
+        // return f(arr, n, k, 0, dp);
+        return f(arr, n, k);
     }
 };
