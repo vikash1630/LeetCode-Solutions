a89122ac407c54229f913804230f938f1c20c96d
Vikash Mundakar <m.vikash1630@gmail.com>
2026-08-20 19:04:40 +0530

    Time: 30 ms (33.5%), Space: 24.5 MB (13.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,27 +3,42 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int islands = 0;
-        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int nr[] = {1, -1, 0, 0};
+        int nc[] = {0, 0, 1, -1};
+        int ans = 0;
         for (int i = 0;i<n;i++) {
             for (int j = 0;j<m;j++) {
-                if (grid[i][j] == '1' && vis[i][j] == 0) {
-                    dfs(n,m,i,j,vis,grid);
-                    islands++;
+                if (vis[i][j] == 1) continue;
+                if (grid[i][j] == '0') {
+                    vis[i][j] = 1;
+                    continue;
+                }
+                // Row, Col
+                ans++;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                vis[i][j] = 1;
+                while (q.size() != 0) {
+                    int p = q.size();
+                    for (int k = 0;k<p;k++) {
+                        auto node = q.front();
+                        q.pop();
+                        int row = node.first;
+                        int col = node.second;
+                        for (int a = 0;a<4;a++) {
+                            int nextRow = row + nr[a];
+                            int nextCol = col + nc[a];
+                            if (nextCol >= 0 && nextRow >= 0 && nextCol < m && nextRow < n && grid[nextRow][nextCol] == '1' && vis[nextRow][nextCol] == 0) {
+                                q.push({nextRow, nextCol});
+                                vis[nextRow][nextCol] = 1;
+                            }
+                        }
+                    }
+                
                 }
             }
         }
-        return islands;
-    }
-
-    void dfs(int n,int m,int row,int col,vector<vector<int>>& vis,vector<vector<char>>& grid) {
-        int drows[] = {-1,0,1,0};
-        int dcols[] = {0,1,0,-1};
-        vis[row][col] = 1;
-        for (int i = 0;i<4;i++) {
-            int nr = row + drows[i];
-            int nc = col + dcols[i];
-            if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && vis[nr][nc] == 0) dfs(n,m,nr,nc,vis,grid);
-        }
+    return ans;
     }
 };
